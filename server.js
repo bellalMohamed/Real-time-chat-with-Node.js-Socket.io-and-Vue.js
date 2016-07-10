7d92f0f36b1462f6477c7297ed87f088887f8e19
B3ll4l <mbellal2000@gmail.com>
2016-07-10 02:21:44 +0200

    App Finised

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(requset, response){
 	response.sendFile(__dirname + '/index.html');
-})
+});
+
+io.on('connection', function (socket) {
+	var user = Date.now();
+
+	socket.on('message.sent', function (message) {
+		io.emit('message', user + ':' + message);
+		console.log(user + ':' + message);
+	});
+
+	io.emit('message', 'User ' + user + ' connected');
+});
 
 http.listen(3000, function () {
-	console.log('hai :D');
+	console.log('Server is up Sir');
 });
